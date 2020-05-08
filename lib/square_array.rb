dd8aa9e4c0a416ab169a702791f7a9a198b6a30f
Zoe Xanos <zxanos@gmail.com>
2020-05-08 09:54:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   count = 0
   new_array = []
   while count < array.length do
-    new_array = Math.sqrt(array[count])
+    new_array += Math.sqrt(array[count])
     count += 1 
   end
   new_array
